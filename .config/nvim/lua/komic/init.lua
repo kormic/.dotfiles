8f48cdb316cbc1fafb98b98b1a7fb6c336eeba95
Michael Kornelakis <kornelakism@gmail.com>
2024-06-25 17:04:12 +0300

    initial commit

---
+++ .config/nvim/lua/komic/init.lua
@@ -0,0 +1,13 @@
+require("komic.options")
+require("komic.remap")
+
+vim.diagnostic.config({
+  virtual_text = {
+    -- source = "always",  -- Or "if_many"
+    prefix = '●', -- Could be '■', '▎', 'x'
+  },
+  severity_sort = true,
+  float = {
+    source = "always",  -- Or "if_many"
+  },
+})
