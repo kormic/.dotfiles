5e46ac6ed1915c0a85d17f31d37ad1f57ba6d67d
Michalis Kornelakis <ycamic@gmail.com>
2024-10-18 17:22:23 +0300

    Add script to toggle opacity and bind ctrl shift T to it, ctrl N for new instance

---
+++ scripts/toggle-opacity.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if grep -q "opacity = 1.0 " ~/.alacritty.toml; then 
+  sed -i '0,/opacity = 1.0/s//opacity = 0.6/' ~/.alacritty.toml 
+else
+  sed -i '0,/opacity = 0.6/s//opacity = 1.0/' ~/.alacritty.toml 
+fi 
