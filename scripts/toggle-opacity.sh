5007cb4ff29102cfa175e71edcf4468ee36b6051
Michalis Kornelakis <ycamic@gmail.com>
2024-10-18 19:03:22 +0300

    Set opacity for alacrity to 1.0 for init, fix the condition in the toggle script

--- scripts/toggle-opacity.sh
+++ scripts/toggle-opacity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if grep -q "opacity = 1.0 " ~/.alacritty.toml; then 
+if grep -q "opacity = 1.0" ~/.alacritty.toml; then 
   sed -i '0,/opacity = 1.0/s//opacity = 0.6/' ~/.alacritty.toml 
 else
   sed -i '0,/opacity = 0.6/s//opacity = 1.0/' ~/.alacritty.toml 
