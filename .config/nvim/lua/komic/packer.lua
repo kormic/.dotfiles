a0f21a84484c85b8081b7f61cc6834737ddfbf9b
Michael Kornelakis <kornelakism@gmail.com>
2025-07-29 12:07:51 +0300

    update telescope remove fugitive

--- .config/nvim/lua/komic/packer.lua
+++ .config/nvim/lua/komic/packer.lua
@@ -9,7 +9,7 @@ return require("packer").startup(function(use)
 
   use({
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.2",
+    tag = "0.1.8",
     -- or                            , branch = '0.1.x',
     requires = { { "nvim-lua/plenary.nvim" } },
   })
@@ -27,7 +27,6 @@ return require("packer").startup(function(use)
   use("nvim-treesitter/playground")
   use("ThePrimeagen/harpoon")
   use("mbbill/undotree")
-  use("tpope/vim-fugitive")
   use({
     "VonHeikemen/lsp-zero.nvim",
     branch = "v2.x",
